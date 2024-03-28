58246c9fd6d72ce6531d188e45871a8d6627e17b
Nikita Iliuschchenko <Nike.il.od@gmail.com>
2024-03-29 00:58:13 +0100

    Add project files.

---
+++ 26.03.24 HW/26.03.24 HW.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <list>
+
+using namespace std;
+
+
+//Task 1
+//struct Point {
+//    int x;
+//    int y;
+//};
+//
+//void Print(Point point) {
+//    cout << point.x << ", " << point.y << ", ";
+//}
+
+
+//Task 2
+struct Point {
+    int x;
+    int y;
+};
+
+void Print(Point point) {
+    cout << point.x << ", " << point.y << ", ";
+}
+
+int main() {
+    //Task 1
+    /*vector<Point> points = { {1, 2}, {4, 4}, {5, 6}, {7, 8}, {9, 10 } };
+
+    for_each(points.begin(), points.end(), Print);*/
+
+
+    //Task 2
+    list<Point> points = { {1, 2}, {3, 4}, {5, 6}, {7, 8}, {9, 10} };
+
+    for_each(points.begin(), points.end(), Print);
+}
